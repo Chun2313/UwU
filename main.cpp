5e1f0dfe42651bfb8d44881dae4c1c8d3e9e360f
25521091-pixel <25521091@gm.uit.edu.vn>
2026-05-12 21:03:59 +0700

    Add fancy line-clearing effects: particles, rainbow glow, screen shake, text popup
    - 600 particles burst from cleared rows with gravity, fade out with glowing cores
    - Rainbow HSV color cycling across cleared rows
    - Expanding concentric glow halos radiating from cleared rows
    - Screen shake with decaying intensity
    - Animated CLEAR!/AWESOME!/PERFECT! text popup with rainbow coloring
    - Full white flash for 3+ line clears (tetris)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "raylib.h"
 #include <ctime>
-#include <vector>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 const int H = 20;
 const int W = 15;
 const int cellSize = 30;
-
-Color colors[] = {LIGHTGRAY, RED, GREEN, BLUE, YELLOW, PURPLE, ORANGE, PINK};
+const int MAX_PARTICLES = 600;
 
 char board[H][W] = {};
 char blocks[][4][4] = {{{' ', 'I', ' ', ' '},
@@ -47,6 +47,52 @@ bool isClearing = false;
 float clearTimer = 0;
 int clearingRows[4];
 int clearingRowCount = 0;
+
+float pX[MAX_PARTICLES], pY[MAX_PARTICLES];
+float pVX[MAX_PARTICLES], pVY[MAX_PARTICLES];
+float pLife[MAX_PARTICLES];
+Color pColor[MAX_PARTICLES];
+int particleCount = 0;
+
+void spawnParticles() {
+    particleCount = 0;
+    for (int k = 0; k < clearingRowCount && particleCount < MAX_PARTICLES; k++) {
+        int row = clearingRows[k];
+        for (int j = 1; j < W - 1 && particleCount < MAX_PARTICLES; j++) {
+            int n = 4 + rand() % 4;
+            for (int p = 0; p < n && particleCount < MAX_PARTICLES; p++) {
+                pX[particleCount] = (j + 0.5f) * cellSize + (rand() % 20 - 10);
+                pY[particleCount] = (row + 0.5f) * cellSize + (rand() % 20 - 10);
+                pVX[particleCount] = (rand() % 400 - 200) * 1.2f;
+                pVY[particleCount] = -(100 + rand() % 400) * 1.2f;
+                pLife[particleCount] = 0.5f + (rand() % 100) * 0.003f;
+                pColor[particleCount] = ColorFromHSV(rand() % 360, 0.9f, 1);
+                particleCount++;
+            }
+        }
+    }
+}
+
+void updateParticles(float dt) {
+    for (int i = 0; i < particleCount; i++) {
+        pX[i] += pVX[i] * dt;
+        pY[i] += pVY[i] * dt;
+        pVY[i] += 400 * dt;
+        pLife[i] -= dt;
+    }
+}
+
+void drawParticles() {
+    for (int i = 0; i < particleCount; i++) {
+        if (pLife[i] > 0) {
+            Color c = pColor[i];
+            c.a = (unsigned char)((pLife[i] / 0.8f) * 255);
+            float size = pLife[i] * 5 + 1;
+            DrawCircle((int)pX[i], (int)pY[i], size, c);
+            DrawCircle((int)pX[i], (int)pY[i], size * 0.4f, Fade(WHITE, c.a / 255.0f * 0.6f));
+        }
+    }
+}
 
 void initBoard() {
     for (int i = 0; i < H; i++)
@@ -102,6 +148,7 @@ int main() {
 
         if (isClearing) {
             clearTimer -= dt;
+            updateParticles(dt);
             if (clearTimer <= 0) {
                 for (int k = 0; k < clearingRowCount; k++)
                     for (int r = clearingRows[k]; r > 0; r--)
@@ -133,6 +180,7 @@ int main() {
                     if (clearingRowCount > 0) {
                         isClearing = true;
                         clearTimer = 1.0f;
+                        spawnParticles();
                     } else {
                         posX = 5;
                         posY = 0;
@@ -145,24 +193,53 @@ int main() {
             }
         }
 
+        float shakeX = 0, shakeY = 0;
+        if (isClearing) {
+            float intensity = clearTimer * 4;
+            shakeX = (rand() % 200 - 100) * intensity / 100.0f;
+            shakeY = (rand() % 200 - 100) * intensity / 100.0f;
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        Color flashColors[] = {WHITE, YELLOW, ORANGE, PINK};
-        int flashIdx = isClearing ? (int)(clearTimer * 20) % 4 : 0;
+        float hue = fmod(clearTimer * 480, 360);
 
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
+                int rx = j * cellSize + (int)shakeX;
+                int ry = i * cellSize + (int)shakeY;
                 if (board[i][j] == '#')
-                    DrawRectangle(j * cellSize, i * cellSize, cellSize - 1,
-                                  cellSize - 1, DARKGRAY);
+                    DrawRectangle(rx, ry, cellSize - 1, cellSize - 1, DARKGRAY);
                 else if (board[i][j] != ' ') {
                     bool isFlashRow = false;
                     for (int k = 0; k < clearingRowCount; k++)
                         if (clearingRows[k] == i) { isFlashRow = true; break; }
-                    DrawRectangle(j * cellSize, i * cellSize, cellSize - 1,
-                                  cellSize - 1,
-                                  isFlashRow && isClearing ? flashColors[flashIdx] : BLUE);
+                    if (isFlashRow && isClearing) {
+                        Color c = ColorFromHSV(hue + i * 30, 1, 1);
+                        DrawRectangle(rx, ry, cellSize - 1, cellSize - 1, c);
+                    } else {
+                        DrawRectangle(rx, ry, cellSize - 1, cellSize - 1, BLUE);
+                    }
+                }
+            }
+        }
+
+        if (isClearing) {
+            for (int k = 0; k < clearingRowCount; k++) {
+                int row = clearingRows[k];
+                float glowSize = (1.0f - clearTimer) * 20;
+                for (int g = 0; g < 3; g++) {
+                    float s = glowSize + g * 4;
+                    Color glow = ColorFromHSV(hue + row * 30, 1, 1);
+                    glow.a = (unsigned char)((60 - g * 15) * (1.0f - clearTimer));
+                    DrawRectangle(
+                        (int)(1 * cellSize - s + shakeX),
+                        (int)(row * cellSize - s + shakeY),
+                        (int)((W - 2) * cellSize + s * 2),
+                        (int)(cellSize + s * 2),
+                        glow
+                    );
                 }
             }
         }
@@ -170,19 +247,41 @@ int main() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 if (blocks[blockType][i][j] != ' ') {
-                    DrawRectangle((posX + j) * cellSize, (posY + i) * cellSize,
+                    DrawRectangle((posX + j) * cellSize + (int)shakeX,
+                                  (posY + i) * cellSize + (int)shakeY,
                                   cellSize - 1, cellSize - 1, RED);
                 }
             }
         }
 
-        for (int i = 0; i < H; i++)
-            DrawLine(0, i * cellSize, W * cellSize, i * cellSize, DARKGRAY);
-        for (int j = 0; j < W; j++)
-            DrawLine(j * cellSize, 0, j * cellSize, H * cellSize, DARKGRAY);
+        for (int i = 0; i <= H; i++)
+            DrawLine(0, i * cellSize + (int)shakeY,
+                     W * cellSize, i * cellSize + (int)shakeY, DARKGRAY);
+        for (int j = 0; j <= W; j++)
+            DrawLine(j * cellSize + (int)shakeX, 0,
+                     j * cellSize + (int)shakeX, H * cellSize, DARKGRAY);
 
-        if (isClearing)
-            DrawRectangle(0, 0, W * cellSize, H * cellSize, Fade(WHITE, 0.15f));
+        drawParticles();
+
+        if (isClearing) {
+            float progress = 1.0f - clearTimer;
+            float alpha = progress < 0.2f ? progress / 0.2f : 1.0f;
+            if (progress > 0.7f) alpha = 1.0f - (progress - 0.7f) / 0.3f;
+            int fontSize = 40 + (int)(progress * 60);
+            const char* labels[] = {"CLEAR!", "AWESOME!", "PERFECT!"};
+            const char* label = labels[clearingRowCount > 1 ? (clearingRowCount > 2 ? 2 : 1) : 0];
+            int tw = MeasureText(label, fontSize);
+            int cx = (W * cellSize - tw) / 2;
+            int cy = (H * cellSize - fontSize) / 2;
+            Color tc = ColorFromHSV(hue, 1, 1);
+            tc.a = (unsigned char)(alpha * 255);
+            DrawText(label, cx + (int)shakeX, cy + (int)shakeY - 30, fontSize, tc);
+
+            if (clearingRowCount >= 3) {
+                Color flash = Fade(WHITE, 0.3f * alpha);
+                DrawRectangle(0, 0, W * cellSize, H * cellSize, flash);
+            }
+        }
 
         EndDrawing();
     }
