68c0dad1d394cc999ec1045f1cd0d45a1da88e7e
25521091-pixel <25521091@gm.uit.edu.vn>
2026-05-12 15:13:19 +0700

    Add block rotation logic

--- main.cpp
+++ main.cpp
@@ -92,6 +92,28 @@ void removeLine() {
     }
 }
 
+void rotateBlock() {
+    char rotated[4][4];
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            rotated[j][3 - i] = blocks[blockType][i][j];
+
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (rotated[i][j] != ' ') {
+                int tx = posX + j;
+                int ty = posY + i;
+                if (tx < 1 || tx >= W - 1 || ty >= H - 1)
+                    return;
+                if (board[ty][tx] != ' ')
+                    return;
+            }
+
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            blocks[blockType][i][j] = rotated[i][j];
+}
+
 int main() {
     InitWindow(W * cellSize, H * cellSize, "Tetris Raylib - MacOS");
     SetTargetFPS(60);
